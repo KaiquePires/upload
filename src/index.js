d42b5a4e260810d61a5c3d829368ef03df4e0496
KaiquePires <manozulukid@gmail.com>
2023-02-15 15:04:45 +0000

    Creating the database container and creating the POST route to index the local files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/upload-db', {
+    useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer.js');
+const Post = require('./models/Post.js');
 
-routes.post('/posts',multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file);
-    res.json({ Hello: 'world' });
+
+
+routes.post('/posts',multer(multerConfig).single('file'), async (req, res) => {
+    const {originalname: name, size, filename: key, url} = req.file;
+    console.log(req.file)
+    const post = ''
+    res.json(post);
 });
 
 module.exports = routes;
